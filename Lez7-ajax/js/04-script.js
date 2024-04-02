24c183024c4e8b3013754eb91f5dec83cd34974f
GioStock <149418816+GioStock@users.noreply.github.com>
2024-04-02 13:55:15 +0200

    aggiornato

---
+++ Lez7-ajax/js/04-script.js
@@ -0,0 +1,8 @@
+const URLUtenti = "http://localhost:3000/utenti";
+fetch(URLUtenti)
+    .then(data =>{
+        return data.json();
+    })
+    .then(utenti=>{
+        console.log(utenti);
+    })
